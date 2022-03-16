7201a5c15a66705f9f237577a8fdd712c8553f99
于沛楠 <2020010877@secoder.net>
2022-03-16 14:10:05 +0800

    Add test cases for func: unpack #3

---
+++ src/unpack_yupeinan/unpack_test.go
@@ -0,0 +1,32 @@
+package unpack
+
+import (
+	"testing"
+)
+
+func TestUnpack(t *testing.T) {
+	resources = append(resources, "car")
+	resources = append(resources, "poem")
+	resources = append(resources, "cars")
+	resources = append(resources, "poems")
+
+	for i := 0; i < len(resources); i++ {
+		filesPath, err := GetFiles("../" + resources[i])
+		if err != nil {
+			if (err.Error() != "open ../" + resources[i] + ": The system cannot find the file specified.") {
+				t.Error(err)
+			}
+		} else {
+			for j := 0; j < len(filesPath); j++ {
+				err = Unpack(filesPath[j], resources[i])
+				if err != nil {
+					t.Error(err)
+				}
+			}
+		}
+	}
+
+
+
+
+}
