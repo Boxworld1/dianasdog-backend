17965c978d284b082678afbd6fa2dbca023c2037
于沛楠 <2020010877@secoder.net>
2022-03-17 12:39:37 +0800

    Add return value 'resouceName' and 'docIDList' in func UnpackXMLFile

--- src/unpackXMLFile.go
+++ src/unpackXMLFile.go
@@ -8,6 +8,8 @@ import (
 	"github.com/beevik/etree"
 )
 
+var currentDocID int = 0
+
 // @title   UnpackXMLFile
 // @description  unpack large XML file to single item
 // @auth    于沛楠       2022/3/16
@@ -15,14 +17,20 @@ import (
 //			resourceName string				the category name of special card
 // @return  itemList     []*etree.Element   XML <item> array (the itemList uses etree from "github.com/beevik/etree")
 //			itemCount    int                total count of <item></item>
+//          docIDList    []int				docID of each item in itemList
+//			resourceName string				the category name of special card
 //		    err          error              non-nil when fileName is wrong
-func UnpackXMLFile(fileName string, resourceName string) (itemList []*etree.Element, itemCount int, err error) {
+func UnpackXMLFile(fileName string, _resourceName string) (itemList []*etree.Element, itemCount int, docIDList []int, resourceName string, err error) {
 	doc := etree.NewDocument()
 	if err := doc.ReadFromFile(fileName); err != nil { //wrong fileName
-		return nil, 0, err
+		return nil, 0, nil, _resourceName, err
 	}
 	root := doc.SelectElement("DOCUMENT")
 	itemList = root.SelectElements("item")
 	itemCount = len(root.SelectElements("item"))
-	return itemList, itemCount, nil
+	for i := 0; i < itemCount; i++ {
+		docIDList = append(docIDList, currentDocID) // 供写入层模拟功能
+		currentDocID++
+	}
+	return itemList, itemCount, docIDList, _resourceName, nil
 }
