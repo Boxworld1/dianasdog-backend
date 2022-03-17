0a2835d26c878ae3a89b274db8fa731b98932733
蒋政 <2020010880@secoder.net>
2022-03-17 14:12:22 +0800

    move dict.go to src/Dict

--- src/Dict/dict.go
+++ src/Dict/dict.go
@@ -10,18 +10,20 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var dataSourceName string = "./dict.db"
+
 // @title   CreateTable
 // @description  create the tables needed
 // @auth    蒋政       2022/3/16
-// @param	table_name   string             the name of the table to be created
+// @param	tableName   string             the name of the table to be created
 // @return  err          error              nil when the table has been created successfully
-func CreateTable(table_name string) error {
-	db, err := sql.Open("sqlite3", "opt/app/data/dict.db")
+func CreateTable(tableName string) error {
+	db, err := sql.Open("sqlite3", dataSourceName)
 	if err != nil {
 		return err
 	}
 	//set the table
-	sql_table := `CREATE TABLE IF NOT EXISTS ` + table_name + `(
+	sql_table := `CREATE TABLE IF NOT EXISTS ` + tableName + `(
 		word VARCHAR(64) PRIMARY KEY NULL
 	);
 	`
@@ -34,21 +36,21 @@ func CreateTable(table_name string) error {
 // @description  insert word into table
 // @auth    蒋政       2022/3/16
 // @param	word		 string				the word to be inserted
-//			table_name   string             the name of the target table
+//			tableName   string             the name of the target table
 // @return  err          error              nil when the word has been inserted into the table successfully
-func Insert(word string, table_name string) error {
-	db, err := sql.Open("sqlite3", "opt/app/data/dict.db")
+func Insert(word string, tableName string) error {
+	db, err := sql.Open("sqlite3", dataSourceName)
 	if err != nil {
 		return err
 	}
 	//judge whether the word has been saved
-	tmp, _ := Search(word, table_name)
+	tmp, _ := Search(word, tableName)
 	if tmp == word {
 		return nil
 	} else {
 
 		//insert word into table
-		insert_task := "INSERT INTO " + table_name + "(word) values(?)"
+		insert_task := "INSERT INTO " + tableName + "(word) values(?)"
 		stmt, err := db.Prepare(insert_task)
 		if err != nil {
 			return err
@@ -65,17 +67,17 @@ func Insert(word string, table_name string) error {
 // @title   Search
 // @description  search word in table
 // @auth    蒋政       2022/3/16
-// @param	word_name		 string				the word to be searched
-//			table_name   string             the name of the target table
+// @param	wordName		 string				the word to be searched
+//			tableName   string             the name of the target table
 // @return  word		 string				the name of the word if it is in the table else "None"
 //			err          error              nil when the word is in the table
-func Search(word_name string, table_name string) (string, error) {
-	db, err := sql.Open("sqlite3", "opt/app/data/dict.db")
+func Search(wordName string, tableName string) (string, error) {
+	db, err := sql.Open("sqlite3", dataSourceName)
 	if err != nil {
 		return "None", err
 	}
 	//search word in table
-	search_task := "SELECT * FROM " + table_name
+	search_task := "SELECT * FROM " + tableName
 	rows, err := db.Query(search_task)
 	if err != nil {
 		return "None", err
@@ -86,9 +88,9 @@ func Search(word_name string, table_name string) (string, error) {
 		if err != nil {
 			return "None", err
 		}
-		if word == word_name {
+		if word == wordName {
 			rows.Close()
-			return word_name, nil
+			return wordName, nil
 		}
 	}
 	rows.Close()
@@ -102,13 +104,13 @@ func Search(word_name string, table_name string) (string, error) {
 // @param	word		 string				the word to be deleted
 //			table_name   string             the name of the target table
 // @return  err          error              nil when the word has been deleted from the table successfully
-func Delete(word string, table_name string) error {
-	db, err := sql.Open("sqlite3", "opt/app/data/dict.db")
+func Delete(word string, tableName string) error {
+	db, err := sql.Open("sqlite3", dataSourceName)
 	if err != nil {
 		return err
 	}
 	//delete word from table
-	delete_task := "delete from " + table_name + " where word=?"
+	delete_task := "delete from " + tableName + " where word=?"
 	stmt, err := db.Prepare(delete_task)
 	if err != nil {
 		return err
