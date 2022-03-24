45dbc590c8a8c41468ff6d471b63f7536fa89057
ziqiao <ziqiao-w20@mails.tsinghua.edu.cn>
2022-03-24 15:48:04 +0800

    try of elastic search

--- src/elastic_search/elastic_search.go
+++ src/elastic_search/elastic_search.go
@@ -15,7 +15,7 @@ type Doc struct {
 	Content string `json:"content"`
 }
 
-func Insert(docId string, content string) {
+func Insert(docId string, content string) (string, error) {
 	client, err := elastic.NewClient(elastic.SetURL("http://127.0.0.1:9200"))
 	if err != nil {
 		// Handle error
@@ -30,12 +30,12 @@ func Insert(docId string, content string) {
 		Do(context.Background())
 	if err != nil {
 		print(err.Error())
-		return
 	}
 	fmt.Printf("Indexed DocId %s to index %s, type %s\n", put1.Id, put1.Index, put1.Type)
+	return put1.Id, err
 }
 
-func Update(docId string, newContent string) {
+func Update(docId string, newContent string) (string, error) {
 	client, err := elastic.NewClient(elastic.SetURL("http://127.0.0.1:9200"))
 	//下面是更新项目的函数，需要传入docid，对数据进行改变
 	put2, err := client.Update().
@@ -45,13 +45,13 @@ func Update(docId string, newContent string) {
 		Do(context.Background())
 	if err != nil {
 		print(err.Error())
-		return
+
 	}
 	fmt.Printf("update content %s\n", put2.Result)
-
+	return put2.Result, err
 }
 
-func Search(content string) { //按照内容去查找，不是精确查找，只要有匹配词就可以
+func Search(content string) ([]Doc, error) { //按照内容去查找，不是精确查找，只要有匹配词就可以
 	client, _ := elastic.NewClient(elastic.SetURL("http://127.0.0.1:9200"))
 	var typ Doc
 	var err error
@@ -60,15 +60,17 @@ func Search(content string) { //按照内容去查找，不是精确查找，只
 	put4, err = client.Search("document").Query(matchPhraseQuery).Do(context.Background())
 	if err != nil {
 		print(err.Error())
-		return
 	}
+	var result []Doc
 	for _, item := range put4.Each(reflect.TypeOf(typ)) { //从搜索结果中取数据的方法，一条条把数据列出来
 		t := item.(Doc)
 		fmt.Printf("%#v\n", t)
+		result = append(result, t)
 	}
+	return result, err
 }
 
-func FetchAll() { //拿到类型document里的所有数据
+func FetchAll() ([]Doc, error) { //拿到类型document里的所有数据
 	client, _ := elastic.NewClient(elastic.SetURL("http://127.0.0.1:9200"))
 	var put3 *elastic.SearchResult
 	var err error
@@ -77,13 +79,15 @@ func FetchAll() { //拿到类型document里的所有数据
 	var typ Doc
 	if err != nil {
 		print(err.Error())
-		return
+
 	}
+	var result []Doc
 	for _, item := range put3.Each(reflect.TypeOf(typ)) { //从搜索结果中取数据的方法
 		t := item.(Doc)
 		fmt.Printf("%#v\n", t)
+		result = append(result, t)
 	}
-
+	return result, err
 }
 
 func Delete(docId string) { //指定想要删除的文档的docId
