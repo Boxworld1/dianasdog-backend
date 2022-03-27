0ffa22d74b32902f9618a20b5b26af4921812180
ziqiao <ziqiao-w20@mails.tsinghua.edu.cn>
2022-03-28 00:12:21 +0800

    上传elastic部分的代码，并加入test部分代码，这只是最初的一些接口，上一次的commit漏掉了一些文件 #2

---
+++ src/elastic_search/elasitic_search_test.go
@@ -0,0 +1,33 @@
+package elastic_search
+
+import (
+	"testing"
+)
+
+func TestInsert(t *testing.T) {
+	_, err := Insert("100", "布加迪威龙")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	_, err := Update("100", "柯尼塞格")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSearch(t *testing.T) {
+	_, err := Search("桑塔纳")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestFetchAll(t *testing.T) {
+	_, err := FetchAll()
+	if err != nil {
+		t.Error(err)
+	}
+}
