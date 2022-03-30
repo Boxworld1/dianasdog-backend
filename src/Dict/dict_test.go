ab15d7e175521a8cdd8d0ad84c9ccaa31ad2b08b
蒋政 <2020010880@secoder.net>
2022-03-30 19:25:03 +0800

    submit the unit test #9

---
+++ src/Dict/dict_test.go
@@ -0,0 +1,65 @@
+package dict
+
+import (
+	"testing"
+)
+
+// test for function: createTable
+func TestCreateTable(t *testing.T) {
+	err := CreateTable("car")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+// test for function: insert
+func TestInsert(t *testing.T) {
+	err := Insert("car", "奔驰")
+	if err != nil {
+		t.Error(err)
+	}
+	err = Insert("car", "奔驰")
+	if err != nil {
+		t.Error(err)
+	}
+	err = Insert("flower", "宝马")
+	if err == nil {
+		t.Error("禁止向不存在的表中插入数据")
+	}
+}
+
+// test for function: search
+func TestSearch(t *testing.T) {
+	tmp, _ := Search("car", "flower")
+	if tmp == "flower" {
+		t.Error("flower is not in car")
+	}
+	tmp, err := Search("car", "奔驰")
+	if tmp != "奔驰" {
+		t.Error("返回错误")
+	}
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
+// test for function: delete
+func TestDelete(t *testing.T) {
+	err := Delete("car", "奔驰")
+	if err != nil {
+		t.Error(err)
+	}
+	err = Delete("flower", "宝马")
+	if err == nil {
+		t.Error("禁止向不存在的表中删除数据")
+	}
+}
+
+// test for function: deleteTable
+func TestDeleteTable(t *testing.T) {
+	err := DeleteTable("car")
+	if err != nil {
+		t.Error(err)
+	}
+}
