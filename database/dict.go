d93328e5e935d5c7bf984dfcd9584e8ec886b6d3
容逸朗 <2020010869@secoder.net>
2022-04-20 00:41:23 +0800

    use new database and deploy test #30

--- database/dict.go
+++ database/dict.go
@@ -1,7 +1,7 @@
-// @Title:  dict
+// @Title: dict
 // @Description: reconstruct the local dictionary with mysql and support the insert, search and delete operations
-// @Author:  蒋政
-// @Update:  2022/3/30
+// @Author: 蒋政
+// @Update: 2022/3/30
 package database
 
 import (
@@ -11,94 +11,189 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var url string = "root:thi4gaiHoa0aicees5booCiet2igoo8i@tcp(mysql.DianasDog.secoder.local:3306)/"
+var db *sql.DB
 
-// var rul string = "root:eelariucie5Tabi8eizioQueineph8la@tcp(localhost:3306)/"
+var dataSourceName string = "root:thi4gaiHoa0aicees5booCiet2igoo8i@tcp(mysql.DianasDog.secoder.local:3306)/dict?charset=utf8"
 
-// @title:	init
+// @title: init
 // @description: connect to the default database
-// @param: do not need in-params
+// @param: do not need a param
 // @return: do not need a return-value
-func CreateDatabase(name string) (*sql.DB, error) {
-	database, err := sql.Open("mysql", url+name+"?charset=utf8")
+func init() {
+	database, err := sql.Open("mysql", dataSourceName)
 	if err != nil {
 		fmt.Println(err)
 	}
-	return database, err
+	db = database
+	inittask := `SET NAMES utf8 `
+	db.Exec(inittask)
 }
 
-// @title:	CreateTableFromDict
+// @title: CreateTableFromDict
 // @description: create the tables needed
-// @param:	tableName	string		the name of the table to be created
-// @return: err			error		nil when the table has been created successfully
-func CreateTableFromDict(db *sql.DB, tableName string) error {
-	createTask := `CREATE TABLE IF NOT EXISTS ` + tableName + `(
-		word VARCHAR(64) PRIMARY KEY NULL
-	)DEFAULT CHARSET=utf8;
-	`
+// @param: tableName    string    the name of the table to be created
+//         columns      []string  the name of the columns to be created in the table
+// @return: err         error     nil when the table has been created successfully
+func CreateTableFromDict(tableName string, columns []string) error {
+	createTask := `CREATE TABLE IF NOT EXISTS ` + tableName + `( id VARCHAR(64) PRIMARY KEY NULL`
+	for i := 1; i < len(columns); i++ {
+		createTask += `,` + columns[i] + ` VARCHAR(64) NULL`
+	}
+	createTask += `)DEFAULT CHARSET=utf8;`
 	_, err := db.Exec(createTask)
 	return err
 }
 
-// @title:	DeleteTableFromDict
+// @title: DeleteTableFromDict
 // @description: Delete the tables mentioned
-// @param:	tableName	string		the name of the table to be deleted
-// @return: err			error			nil when the table has been deleted successfully
-func DeleteTableFromDict(db *sql.DB, tableName string) error {
+// @param: tableName    string    the name of the table to be deleted
+// @return: err         error     nil when the table has been deleted successfully
+func DeleteTableFromDict(tableName string) error {
 	deleteTask := `DROP TABLE ` + tableName
 	_, err := db.Exec(deleteTask)
 	return err
 }
 
-// @title:	InsertToDict
-// @description:  insert word into table
-// @param:	tableName   string      the name of the target table
-//			word		string		the word to be inserted
-// @return: err         error      nil when the word has been inserted into the table successfully
-func InsertToDict(db *sql.DB, tableName string, word string) error {
-	insertTask := "INSERT IGNORE INTO " + tableName + "(word) values(?)"
-	stmt, err := db.Prepare(insertTask)
+// @title: ShowTablesInDict
+// @description: get all the tablenames in the dict
+// @param: No param is needed.
+// @return: tables      []string  store the name of all the tables
+//		    err         error     nil when the name of the tables has been read successfully
+func ShowTablesInDict() ([]string, error) {
+	task := "select table_name from information_schema.tables where table_schema = 'dict'"
+	rows, err := db.Query(task)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	_, err = stmt.Exec(word)
+	var tableName string
+	tables := []string{}
+	for rows.Next() {
+		err = rows.Scan(&tableName)
+		if err != nil {
+			return nil, err
+		}
+		tables = append(tables, tableName)
+	}
+	return tables, nil
+}
+
+// @title: ShowColumnsInTables
+// @description: get all the columnnames in the table
+// @param: tableName    string    the name of the table to be searched
+// @return: columns     []string  store the name of all the columns
+//		    err         error     nil when the name of the columns has been read successfully
+func ShowColumnsInTable(tableName string) ([]string, error) {
+	task := "select column_name from information_schema.COLUMNS where table_name='" + tableName + "' and table_schema='dict'"
+	rows, err := db.Query(task)
+	if err != nil {
+		return nil, err
+	}
+	var columnName string
+	columns := []string{}
+	for rows.Next() {
+		err = rows.Scan(&columnName)
+		if err != nil {
+			return nil, err
+		}
+		columns = append(columns, columnName)
+	}
+	return columns, nil
+}
+
+// @title: InsertToDict
+// @description: insert word into table
+// @param: tableName    string    the name of the target table
+//         words        []string  the word to be inserted
+// @return: err         error     nil when the word has been inserted into the table successfully
+func InsertToDict(tableName string, words []string) error {
+	insertTask := "INSERT IGNORE INTO " + tableName + "(id"
+	columns, _ := ShowColumnsInTable(tableName)
+	for i := 1; i < len(columns); i++ {
+		insertTask += "," + columns[i]
+	}
+	insertTask += ") values(?"
+	for i := 0; i < len(columns)-1; i++ {
+		insertTask += ",?"
+	}
+	insertTask += ")"
+
+	var interfaceSlice []interface{} = make([]interface{}, len(words))
+	for i, d := range words {
+		interfaceSlice[i] = d
+	}
+
+	_, err := db.Exec(insertTask, interfaceSlice...)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-// @title:   SearchFromDict
-// @description:  search word from table
-// @param:	tableName   string      the name of the target table
-//			word		string		the word to be searched
-// @return: word		 string		the name of the word if it is in the table else "None"
-//			err          error      nil when the word is in the table
-func SearchFromDict(db *sql.DB, tableName string, word string) (string, error) {
-	selectTask := "select word from " + tableName + " where word=?"
-	var res string
-	err := db.QueryRow(selectTask, word).Scan(&res)
-	if err == nil && res == word {
-		return res, err
-	} else {
-		return "None", err
+// @title: SearchFromDict
+// @description: search word from table
+// @param: tableName    string    the name of the target table
+//         id          string    the id of the data to be searched
+// @return: res         []string  the result of the search
+//          err         error     nil when the word has been successfully searched in the table
+func SearchFromDict(tableName string, id string) ([]string, error) {
+	//selectTask := "select id from " + tableName + " where id=?"
+	columns, _ := ShowColumnsInTable(tableName)
+	selectTask := "select id"
+	for i := 1; i < len(columns); i++ {
+		selectTask += ", " + columns[i]
 	}
+	selectTask += " from " + tableName + " where id=?"
+	fmt.Println(selectTask)
+	var interfaceSlice []interface{} = make([]interface{}, len(columns))
+	var tmp [10]string
+	for i := 0; i < len(columns); i++ {
+		interfaceSlice[i] = &tmp[i]
+	}
+	err := db.QueryRow(selectTask, id).Scan(interfaceSlice...)
+	if tmp[0] != id {
+		return nil, err
+	}
+	var res []string = make([]string, len(columns))
+	for i := 0; i < len(columns); i++ {
+		res[i] = tmp[i]
+	}
+	return res, err
 }
 
-// @title:	DeleteFromDict
-// @description:	delete word from table
-// @param:	word		 string		the word to be deleted
-//			table_name   string     the name of the target table
-// @return: err          error      nil when the word has been deleted from the table successfully
-func DeleteFromDict(db *sql.DB, tableName string, word string) error {
-	deleteTask := "delete from " + tableName + " where word=?"
-	stmt, err := db.Prepare(deleteTask)
-	if err != nil {
-		return err
-	}
-	_, err = stmt.Exec(word)
+// @title: DeleteFromDict
+// @description: delete word from table
+// @param: tableName  string    the name of the target table
+//         id         string    the id of the data to be deleted
+// @return:err        error     nil when the word has been deleted from the table successfully
+func DeleteFromDict(tableName string, id string) error {
+	deleteTask := "delete from " + tableName + " where id=?"
+	_, err := db.Exec(deleteTask, id)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+// @title: QueryColumn
+// @description: Get all the word in column
+// @param: table_name    string    the name of the target table
+//         column        string    the name of the column
+// @return: dictionary    []string    store the total word in the table
+//          err           error       nil when the word has been deleted from the table successfully
+func QueryColumn(tableName string, column string) ([]string, error) {
+	QueryTask := "select " + column + " from " + tableName
+	rows, err := db.Query(QueryTask)
+	if err != nil {
+		return nil, err
+	}
+	var word string
+	dictionary := []string{}
+	for rows.Next() {
+		err = rows.Scan(&word)
+		if err != nil {
+			return nil, err
+		}
+		dictionary = append(dictionary, word)
+	}
+	return dictionary, nil
 }
--- database/dict_test.go
+++ database/dict_test.go
@@ -1,67 +1,111 @@
-// @title	TestDictInterface
-// @description	检查 sql 的功能
-// @auth	jz		2022/3/30
-// @auth	ryl		2022/4/19	17:30
-// @param	t		*testing.T
-
 package database
 
 import (
 	"testing"
 )
 
-func TestDictInterface(t *testing.T) {
-	// 创建数据库 dict
-	db, err := CreateDatabase("dict")
+// test for function: createTable
+func TestCreateTableFromDict(t *testing.T) {
+	d := []string{"id", "title"}
+	err := CreateTableFromDict("car", d)
 	if err != nil {
 		t.Error(err)
 	}
+}
 
-	// 新建表格
-	err = CreateTableFromDict(db, "car")
+//test for function: showtablesindict
+func TestShowTablesInDict(t *testing.T) {
+	dict, err := ShowTablesInDict()
 	if err != nil {
 		t.Error(err)
 	}
+	var exist = false
+	for _, tmp := range dict {
+		if tmp == "car" {
+			exist = true
+			break
+		}
+	}
+	if !exist {
+		t.Error("返回错误")
+	}
+}
 
-	// 插入数据
-	err = InsertToDict(db, "car", "奔驰")
+//test for function:ShowColumnsInTable
+func TestShowColumnsInTable(t *testing.T) {
+	dict, err := ShowColumnsInTable("car")
 	if err != nil {
 		t.Error(err)
 	}
-	err = InsertToDict(db, "car", "奔驰")
+	if dict[0] != "id" || dict[1] != "title" {
+		t.Error("返回错误")
+	}
+}
+
+// test for function: insert
+func TestInsertToDict(t *testing.T) {
+	item := []string{"test0", "奔驰"}
+	err := InsertToDict("car", item)
 	if err != nil {
 		t.Error(err)
 	}
-	err = InsertToDict(db, "flower", "宝马")
+	err = InsertToDict("car", item)
+	if err != nil {
+		t.Error(err)
+	}
+	item = []string{"test1", "宝马"}
+	err = InsertToDict("flower", item)
 	if err == nil {
 		t.Error("禁止向不存在的表中插入数据")
 	}
+}
 
-	// 查找数据
-	tmp, _ := SearchFromDict(db, "car", "flower")
-	if tmp == "flower" {
-		t.Error("flower is not in car")
+// test for function: search
+func TestSearchFromDict(t *testing.T) {
+	tmp, _ := SearchFromDict("car", "test2")
+	if len(tmp) != 0 {
+		t.Error("test2 is not in car")
 	}
-	tmp, err = SearchFromDict(db, "car", "奔驰")
-	if tmp != "奔驰" {
+	tmp, err := SearchFromDict("car", "test0")
+	if tmp[0] != "test0" || tmp[1] != "奔驰" {
 		t.Error("查询失败")
 	}
 	if err != nil {
 		t.Error(err)
 	}
+}
 
-	// 删除数据
-	err = DeleteFromDict(db, "car", "奔驰")
+//test for function QueryColumn
+func TestQueryColumn(t *testing.T) {
+	item := []string{"test1", "宝马"}
+	InsertToDict("car", item)
+	dictionary, err := QueryColumn("car", "title")
 	if err != nil {
 		t.Error(err)
 	}
-	err = DeleteFromDict(db, "flower", "宝马")
+	check1 := (dictionary[0] == "奔驰" && dictionary[1] == "宝马")
+	check2 := (dictionary[0] == "宝马" && dictionary[1] == "奔驰")
+	if !check1 && !check2 {
+		t.Error("返回错误")
+	}
+
+}
+
+// test for function: delete
+func TestDeleteFromDict(t *testing.T) {
+	err := DeleteFromDict("car", "test1")
+	if err != nil {
+		t.Error(err)
+	}
+	err = DeleteFromDict("flower", "宝马")
 	if err == nil {
 		t.Error("禁止向不存在的表中删除数据")
 	}
+}
 
-	// 删除表格
-	err = DeleteTableFromDict(db, "car")
+// test for function: deleteTable
+func TestDeleteTableFromDict(t *testing.T) {
+	err := DeleteTableFromDict("car")
 	if err != nil {
 		t.Error(err)
 	}
