1187b609ba145d5a3e4b4bee0d923c5ac672c8e0
蒋政 <2020010880@secoder.net>
2022-04-19 22:51:59 +0800

    submit the dict.go #31

---
+++ database/dict.go
@@ -0,0 +1,191 @@
+// @Title: dict
+// @Description: reconstruct the local dictionary with mysql and support the insert, search and delete operations
+// @Author: 蒋政
+// @Update: 2022/3/30
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+//var dataSourceName string = "root:thi4gaiHoa0aicees5booCiet2igoo8i@tcp(mysql.DianasDog.secoder.local:3306)/dict?charset=utf8"
+var dataSourceName string = "root:root@tcp(0.0.0.0:49153)/dict?charset=utf8"
+
+// @title: init
+// @description: connect to the default database
+// @param: do not need a param
+// @return: do not need a return-value
+func init() {
+	database, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		fmt.Println(err)
+	}
+	db = database
+	inittask := `SET NAMES utf8 `
+	db.Exec(inittask)
+}
+
+// @title: CreateTableFromDict
+// @description: create the tables needed
+// @param: tableName    string    the name of the table to be created
+//         columns      []string  the name of the columns to be created in the table
+// @return: err         error     nil when the table has been created successfully
+func CreateTableFromDict(tableName string, columns []string) error {
+	createTask := `CREATE TABLE IF NOT EXISTS ` + tableName + `(`
+	for i := 0; i < len(columns); i++ {
+		createTask += columns[i] + ` VARCHAR(64) NULL, `
+	}
+	createTask += `PRIMARY KEY(key) )DEFAULT CHARSET=utf8;`
+	_, err := db.Exec(createTask)
+	return err
+}
+
+// @title: DeleteTableFromDict
+// @description: Delete the tables mentioned
+// @param: tableName    string    the name of the table to be deleted
+// @return: err         error     nil when the table has been deleted successfully
+func DeleteTableFromDict(tableName string) error {
+	deleteTask := `DROP TABLE ` + tableName
+	_, err := db.Exec(deleteTask)
+	return err
+}
+
+// @title: ShowTablesInDict
+// @description: get all the tablenames in the dict
+// @param: No param is needed.
+// @return: tables      []string  store the name of all the tables
+//		    err         error     nil when the table has been deleted successfully
+func ShowTablesInDict() ([]string, error) {
+	task := "select table_name from information_schema.tables where table_schema = 'dict'"
+	rows, err := db.Query(task)
+	if err != nil {
+		return nil, err
+	}
+	var tableName string
+	tables := []string{}
+	for rows.Next() {
+		err = rows.Scan(&tableName)
+		if err != nil {
+			return nil, err
+		}
+		tables = append(tables, tableName)
+	}
+	return tables, nil
+}
+
+// @title: ShowColumnsInTables
+// @description: get all the columnnames in the table
+// @param: tableName    string    the name of the table to be searched
+// @return: columns     []string  store the name of all the columns
+//		    err         error     nil when the table has been deleted successfully
+func ShowColumnsInTable(tableName string) ([]string, error) {
+	task := "select group_concat(column_name) from information_schema.COLUMNS where table_name=" + tableName + " and table_schema='dict'"
+	rows, err := db.Query(task)
+	if err != nil {
+		return nil, err
+	}
+	var columnName string
+	columns := []string{}
+	for rows.Next() {
+		err = rows.Scan(&columnName)
+		if err != nil {
+			return nil, err
+		}
+		columns = append(columns, columnName)
+	}
+	return columns, nil
+}
+
+// @title: InsertToDict
+// @description: insert word into table
+// @param: tableName    string    the name of the target table
+//         words        []string    the word to be inserted
+// @return: err         error     nil when the word has been inserted into the table successfully
+func InsertToDict(tableName string, words []string) error {
+	insertTask := "INSERT IGNORE INTO " + tableName + "("
+	columns, _ := ShowColumnsInTable(tableName)
+	for i := 0; i < len(columns); i++ {
+		insertTask += columns[i] + ","
+	}
+	insertTask += ") values(?"
+	for i := 0; i < len(columns)-1; i++ {
+		insertTask += ",?"
+	}
+	insertTask += ")"
+
+	var interfaceSlice []interface{} = make([]interface{}, len(words))
+	for i, d := range words {
+		interfaceSlice[i] = d
+	}
+
+	_, err := db.Exec(insertTask, interfaceSlice...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// @title: SearchFromDict
+// @description: search word from table
+// @param: tableName    string    the name of the target table
+//         key          string    the key of the data to be searched
+// @return: res         []string  the result of the search
+//          err         error     nil when the word is in the table
+func SearchFromDict(tableName string, key string) ([]string, error) {
+	selectTask := "select key from " + tableName + " where key=?"
+	columns, _ := ShowColumnsInTable(tableName)
+	var interfaceSlice []interface{} = make([]interface{}, len(columns))
+	var res []string = make([]string, len(columns))
+	for i, d := range res {
+		interfaceSlice[i] = &d
+	}
+	err := db.QueryRow(selectTask, key).Scan(interfaceSlice...)
+	if err == nil && res[0] == key {
+		return res, err
+	} else {
+		return nil, err
+	}
+}
+
+// @title: DeleteFromDict
+// @description: delete word from table
+// @param: tableName    string    the name of the target table
+//         key         string    the key of the data to be deleted
+// @return:err         error     nil when the word has been deleted from the table successfully
+func DeleteFromDict(tableName string, key string) error {
+	deleteTask := "delete from " + tableName + " where key=?"
+	_, err := db.Exec(deleteTask, key)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// @title: QueryColumn
+// @description: Get all the word in column
+// @param: table_name    string    the name of the target table
+//         column        string    the name of the column
+// @return: dictionary    []string    store the total word in the table
+//          err           error       nil when the word has been deleted from the table successfully
+func QueryColumn(tableName string, column string) ([]string, error) {
+	QueryTask := "select " + column + " from " + tableName
+	rows, err := db.Query(QueryTask)
+	if err != nil {
+		return nil, err
+	}
+	var word string
+	dictionary := []string{}
+	for rows.Next() {
+		err = rows.Scan(&word)
+		if err != nil {
+			return nil, err
+		}
+		dictionary = append(dictionary, word)
+	}
+	return dictionary, nil
+}
