f6a9dbb4428aeb1fdcc87fcfef5272fbef5598ed
容逸朗 <2020010869@secoder.net>
2022-04-26 13:09:07 +0800

    re-deploy as secoder platform problem #39

--- database/local_var.go
+++ database/local_var.go
@@ -2,7 +2,7 @@ package database
 
 // For local test
 
-// var SqlUrl string = "root:hieyohrahng0eing7nuaBoofaichaiBo@tcp(localhost:3306)/"
+// var SqlUrl string = "root:ahsh4ciToh6chohkukah1iib7thuapo7@tcp(localhost:3306)/"
 // var EsUrl string = "http://localhost:9200"
 // var RedisUrl string = "localhost:6379"
 // var RedisPw string = "0"
